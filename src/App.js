8f46ffbcfa286499929d45ac3f69312f12fbc094
vandana saini <vandanasaini3010@gmail.com>
2023-12-11 17:55:12 +0530

    add close

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React,{Fragment} from  'react';
+import React,{Fragment,useState} from  'react';
 import Header from './Component/Layout/Header';
 import Meals from './Component/Meal/Meals';
 
 import Cart from './Component/Cart/Cart';
 const App=()=>{
+    const[cartShown,setCart]=useState(false);
+    const cartHandler=()=>{
+        setCart(true);
+    };
+    const HideCart=()=>{
+        setCart(false);
+    };
+
     return(
     
     <Fragment>
-         <Cart/>
-        <Header/>
+        { cartShown && <Cart/>}
+        <Header onShowcart={cartHandler} onClose={HideCart}/>
 
         <main>
         <Meals/>
